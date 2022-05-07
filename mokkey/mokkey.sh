9362272a29d78ffe7872956df64b17259947cf2b
Melvin Kool <melvin@melvinkool.nl>
2022-05-07 20:11:03 +0200

    Sign NVIDIA drivers

--- mokkey/mokkey.sh
+++ mokkey/mokkey.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 keyname="MOK"
+SIGN_FILE="/usr/src/linux-obj/x86_64/default/scripts/sign-file"
+OUT_DIR="."
+PRIV_FILE="${OUT_DIR}/$keyname.priv"
+DER_FILE="${OUT_DIR}/$keyname.der"
+
 if [ "$1" = "--gen" ]; then
-	openssl req -x509 -new -nodes -utf8 -sha256 -days 36500 -batch -config openssl.cnf -outform DER -out "$keyname.der"  -keyout "$keyname.priv"
-	mokutil --import "$keyname.der"
+	openssl req -x509 -new -nodes -utf8 -sha256 -days 36500 -batch -config openssl.cnf -outform DER -out "$DER_FILE"  -keyout "$PRIV_FILE"
+	mokutil --import "$DER_FILE"
 	echo "Please reboot your system and enroll MOK"
 elif [ "$1" = "--sign" ]; then
 	echo "signing key..."
 	# VirtualBox
-	"/usr/src/kernels/$(uname -r)/scripts/sign-file" sha256 "./$keyname.keyout" "./$keyname.der" "$(modinfo -n vboxdrv)"
-	"/usr/src/kernels/$(uname -r)/scripts/sign-file" sha256 "./$keyname.keyout" "./$keyname.der" "$(modinfo -n vboxnetflt)"
-	"/usr/src/kernels/$(uname -r)/scripts/sign-file" sha256 "./$keyname.keyout" "./$keyname.der" "$(modinfo -n vboxnetadp)"
-	"/usr/src/kernels/$(uname -r)/scripts/sign-file" sha256 "./$keyname.keyout" "./$keyname.der" "$(modinfo -n vboxpci)"
+	#"$SIGN_FILE" sha256 "$PRIV_FILE" "$DER_FILE" "$(modinfo -n vboxdrv)"
+	#"$SIGN_FILE" sha256 "$PRIV_FILE" "$DER_FILE" "$(modinfo -n vboxnetflt)"
+	#"$SIGN_FILE" sha256 "$PRIV_FILE" "$DER_FILE" "$(modinfo -n vboxnetadp)"
+	#"$SIGN_FILE" sha256 "$PRIV_FILE" "$DER_FILE" "$(modinfo -n vboxpci)"
 	# VMWare
-	"/usr/src/kernels/$(uname -r)/scripts/sign-file" sha256 "./$keyname.keyout" "./$keyname.der" "$(modinfo -n vmmon)"
-	"/usr/src/kernels/$(uname -r)/scripts/sign-file" sha256 "./$keyname.keyout" "./$keyname.der" "$(modinfo -n vmnet)"
-fi
+	#"$SIGN_FILE" sha256 "$PRIV_FILE" "$DER_FILE" "$(modinfo -n vmmon)"
+	#"$SIGN_FILE" sha256 "$PRIV_FILE" "$DER_FILE" "$(modinfo -n vmnet)"
 
+	# NVIDIA
+	"$SIGN_FILE" sha256 "$PRIV_FILE" "$DER_FILE" "$(modinfo -n nvidia_drm)"
+	"$SIGN_FILE" sha256 "$PRIV_FILE" "$DER_FILE" "$(modinfo -n nvidia_modeset)"
+	"$SIGN_FILE" sha256 "$PRIV_FILE" "$DER_FILE" "$(modinfo -n nvidia_uvm)"
+	"$SIGN_FILE" sha256 "$PRIV_FILE" "$DER_FILE" "$(modinfo -n nvidia-peermem)"
+	"$SIGN_FILE" sha256 "$PRIV_FILE" "$DER_FILE" "$(modinfo -n nvidia)"
+fi
